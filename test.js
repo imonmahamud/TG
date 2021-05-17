2e6f29a70c374004e7b29c6f8854997bd8452887
Mahamud Hasan Imon <imonmahamud495@gmail.com>
2021-05-17 20:09:37 +0600

    Added new function for array reverse

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
+//array traverse and sum the array value by recursive function in javascript
 var arr=[1,2,3,4]
-function arrayTraverseSum(arr,lastindex){
-    if(lastindex<0){
+function arrayTraverseSum(arr,lastIndex){
+    if(lastIndex<0){
         return 0
     }
-    return arr[lastindex]+arrayTraverseSum(arr,lastindex-1)
+    return arr[lastIndex]+arrayTraverseSum(arr,lastIndex-1)
 }
 console.log(arrayTraverseSum(arr,arr.length-1))
+//this is for array traverse and reverse array by recursive function in javascript
+var arr=[1,2,3,4,5,6]
+function arrayReverse(arr,firstIndex,lastIndex){
+    if(firstIndex>lastIndex){
+        return
+    }
+    var temp=arr[firstIndex]
+    arr[firstIndex]=arr[lastIndex]
+    arr[lastIndex]=temp
+    arrayReverse(arr,firstIndex+1,lastIndex-1)
+    return arr
+}
+console.log(arrayReverse(arr,0,arr.length-1))
