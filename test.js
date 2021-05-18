96852d201e97692839e562278fb8b32f8adcf828
Mahamud Hasan Imon <imonmahamud495@gmail.com>
2021-05-18 18:03:13 +0600

    I just add pure function here

--- test.js
+++ test.js
@@ -1,30 +1,7 @@
-//array traverse and sum the array value by recursive function in javascript
-var arr=[1,2,3,4]
-function arrayTraverseSum(arr,lastIndex){
-    if(lastIndex<0){
-        return 0
-    }
-    return arr[lastIndex]+arrayTraverseSum(arr,lastIndex-1)
+// pure function 
+//same output for same args until change
+//don't conflict another part of the code
+function sqr(n){
+    return n*n
 }
-console.log(arrayTraverseSum(arr,arr.length-1))
-//this is for array traverse and reverse array by recursive function in javascript
-var arr=[1,2,3,4,5,6]
-function arrayReverse(arr,firstIndex,lastIndex){
-    if(firstIndex>lastIndex){
-        return
-    }
-    var temp=arr[firstIndex]
-    arr[firstIndex]=arr[lastIndex]
-    arr[lastIndex]=temp
-    arrayReverse(arr,firstIndex+1,lastIndex-1)
-    return arr
-}
-console.log(arrayReverse(arr,0,arr.length-1))
-//factorial By recursive function 
-function fact(n){
-    if(n==1){
-        return 1
-    }
-    return n*fact(n-1)
-}
-console.log(fact(5))
+console.log(sqr(3))
