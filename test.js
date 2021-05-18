fa0d1054fad4a9963561fe5a58336477ef57c52b
Mahamud Hasan Imon <imonmahamud495@gmail.com>
2021-05-18 18:34:32 +0600

    Veriation of callback function

--- test.js
+++ test.js
@@ -1,29 +1,20 @@
-//array traverse and sum the array value by recursive function in javascript
-var arr=[1,2,3,4,5,6,7,8]
-function arrayTraverseSum(arr,lastIndex){
-    if(lastIndex<0){
-        return 0
+//callback with a function
+var me={
+    name:'MH Imon',
+    age:25,
+    email:'imonmahamud495@gmail.com'
+};
+function printMySelf(person,callback){
+    console.log('Name :'+person.name+' ('+person.age+')');
+    
+    if(person.age>=18){
+        callback(person.email)
+    }else{
+        console.log('You are too little');
+        
     }
-    return arr[lastIndex]+arrayTraverseSum(arr,lastIndex-1)
 }
-console.log(arrayTraverseSum(arr,arr.length-1))
-//this is for array traverse and reverse array by recursive function in javascript
-var arr=[1,2,3,4,5,6,7,8,9,10,11,12]
-function arrayReverse(arr,firstIndex,lastIndex){
-    if(firstIndex>lastIndex){
-        return
-    }
-    var temp=arr[firstIndex]
-    arr[firstIndex]=arr[lastIndex]
-    arr[lastIndex]=temp
-    arrayReverse(arr,firstIndex+1,lastIndex-1)
-    return arr
-}
-console.log(arrayReverse(arr,0,arr.length-1))
-// pure function 
-//same output for same args until change
-//don't conflict another part of the code
-function sqr(n){
-    return n*n
-}
-console.log(sqr(3))
+printMySelf(me,function(email){
+    console.log('Email sent to :'+email);
+    
+})
